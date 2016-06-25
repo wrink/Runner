8be4b59c4fcacd4b6806f22aa05e500982adf999
David Wikler <david@Davids-MBP.home>
2016-06-24 23:01:31 -0400

    initalized Runner

---
+++ Runner.js
@@ -0,0 +1,74 @@
+var run = function (url, args, callback, callbackOnErr) {
+	var out = function (exit, call, inp, err) {
+		return {
+			"exitCode": exit,
+			"callbackReturn": call,
+			"inputArray": inp,
+			"errorArray": err
+		};
+	};
+
+	imports = new JavaImporter(java.net, java.io, java.lang.Runtime)
+	with(imports) {
+
+		var input = new BufferedReader(new InputStreamReader(new URL(url).openStream()));
+
+		var name = url.split('/');
+		name = name[name.length - 1];
+
+		var holder;
+		var contents = input.readLine();
+		while((holder = input.readLine()) !== null) contents += '\n' + holder;
+
+		var tmp = File.createTempFile(name, '.tmp');
+		tmp.setExecutable(true);
+
+		var writer = new FileWriter(tmp);
+		writer.write(contents);
+		writer.flush();
+		writer.close();
+
+		try {
+			var process = Runtime.getRuntime().exec([tmp.getAbsolutePath()].concat(args || []));
+			process.waitFor();
+
+			input = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			error = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+
+			var errFlag = false;
+
+			var inputArray = [];
+			var errorArray = [];
+
+			while ((holder = input.readLine()) !== null) {
+				print(holder);
+				inputArray.push(holder);
+			}
+			while ((holder = error.readLine()) !== null) {
+				errFlag = true;
+
+				print(holder);
+				errorArray.push(holder);
+			}
+
+			tmp.delete();
+
+			if (errFlag) {
+				if (callback && callbackOnErr) return out(-1, callback(out(-1, null, inputArray, errorArray)), inputArray, errorArray);
+				else return out(-1, null, inputArray, errorArray);
+			} else {
+				if (callback) return out(0, callback(out(0, null, inputArray, [])), inputArray, []);
+				else return out(0, null, inputArray, []);
+			}
+		} catch (e) {
+			print(e);
+
+			tmp.delete();
+
+			if (callback && callbackOnErr) return out(1, callback(out(-1, null, [], e.toString().split('\n'))), [], e.toString().split('\n'));
+			else return out(1, null, [], e.toString().split('\n'));
+		}
+	}
+};
+
+run('https://raw.githubusercontent.com/wrink/executables/master/test.js', [1, 2, 3]);
